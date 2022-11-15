7a8f74daea900974ba6032a5c8f42bfcf4f9a8d4
정혜주 <fixup719@gmail.com>
2022-11-15 14:04:43 +0900

    vuetify 설정

---
+++ src/components/common/NavView.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon>안녕</v-app-bar-nav-icon>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
