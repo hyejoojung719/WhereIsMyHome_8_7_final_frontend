3a3c6d468e3f0d83437bf15202ad6878286b984b
lny7036 <lny7036@naver.com>
2022-11-15 17:05:33 +0900

    FooterView #2 - 글꼴 지정, 배경, 수평선 생성

--- src/App.vue
+++ src/App.vue
@@ -21,4 +21,28 @@ export default {
 ::v-deep .v-application--wrap {
   min-height: fit-content;
 }
+
+.v-application {
+  font-family: "NanumSquareRound", sans-serif !important;
+}
+@font-face {
+  font-family: "NanumSquareRound";
+  src: url("assets/fonts/NanumSquareRoundOTFR.otf") format("opentype");
+  font-weight: 400;
+}
+@font-face {
+  font-family: "NanumSquareRound";
+  src: url("assets/fonts/NanumSquareRoundOTFL.otf") format("opentype");
+  font-weight: 300;
+}
+@font-face {
+  font-family: "NanumSquareRound";
+  src: url("assets/fonts/NanumSquareRoundOTFB.otf") format("opentype");
+  font-weight: 700;
+}
+@font-face {
+  font-family: "NanumSquareRound";
+  src: url("assets/fonts/NanumSquareRoundOTFEB.otf ") format("opentype");
+  font-weight: 700;
+}
 </style>
--- src/components/common/FooterView.vue
+++ src/components/common/FooterView.vue
@@ -1,23 +1,32 @@
 <template>
   <v-footer padless>
     <v-card
+      fixed
       tile
       flat
-      color="red accent-2"
       width="100%"
       height="120px"
-      absolute
       class="d-flex align-center justify-center"
     >
-      <v-row class="pl-3">
-        <v-btn v-for="link in links" :key="link" text class="my-2">{{
-          link
-        }}</v-btn>
-
-        <v-col class="primary py-4" cols="12">
-          <v-row> 이나연 GIT 주소 | 정혜주 GIT 주소 </v-row>
-          <v-row> 대전광역시 유성구 덕명동 124 | 042-820-7400 </v-row>
-          <v-row> Copyright &copy;SSAFY_CarrotCheezeCake</v-row>
+      <v-row class="pl-3" height="100%">
+        <v-col class="pa-0" cols="12">
+          <v-divider></v-divider>
+        </v-col>
+        <v-col class="px-3 py-0" cols="12">
+          <v-btn v-for="link in links" :key="link" text class="my-2">{{
+            link
+          }}</v-btn>
+        </v-col>
+        <v-col
+          class="py-2 body-2 grey--text text--darken-1"
+          style="font-color: #767171"
+          cols="12"
+        >
+          <v-row class="px-7"> 이나연 GIT 주소 | 정혜주 GIT 주소 </v-row>
+          <v-row class="px-7">
+            대전광역시 유성구 덕명동 124 | 042-820-7400
+          </v-row>
+          <v-row class="px-7"> Copyright &copy;SSAFY_CarrotCheezeCake</v-row>
         </v-col>
       </v-row>
     </v-card>
