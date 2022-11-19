f17c45999dca06318fa9b2fe3219fc71b9300d64
lny7036 <lny7036@naver.com>
2022-11-19 19:21:56 +0900

    NavView Logout 구현 및 마이페이지 css, 이동 가능

--- src/components/common/NavView.vue
+++ src/components/common/NavView.vue
@@ -11,14 +11,34 @@
     <router-link :to="{ name: 'home' }">
       <span class="white--text px-5">부동산 뉴스</span>
     </router-link>
-    <router-link :to="{ name: 'userSignIn' }">
+    <router-link :to="{ name: 'userSignIn' }" v-if="!!!userInfo.id">
       <span class="white--text px-5">로그인</span>
     </router-link>
+    <template v-else>
+      <router-link :to="{ name: 'userMyPage', params: { user_id: userInfo.id } }">
+        <span class="white--text px-5">마이페이지</span>
+      </router-link>
+      <v-btn class="white--text px-5" href="#" @click.prevent="signout" style="font-size: 16px" text>로그아웃</v-btn>
+      <!-- <li class="nav-item">
+        <span>{{ userInfo.id }}님 환영합니다.</span>
+      </li> -->
+    </template>
   </v-app-bar>
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState("userStore", ["userInfo"]),
+  },
+  methods: {
+    async signout() {
+      await this.$store.dispatch("userStore/signOut");
+      this.$router.replace({ name: "main" }).catch(() => {}); //홈 화면 이동
+    },
+  },
+};
 </script>
 
 <style scoped>
